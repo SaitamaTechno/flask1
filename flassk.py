2b88f2f4fa515996d496245475ed43770d9f4ed6
SaitamaTechno <halukenestelli143@gmail.com>
2023-01-30 16:28:08 +0000

    added host

--- flassk.py
+++ flassk.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 @app.route('/')
 
 def hello_world():
-    return 'Hello World'
+    return 'Hello Saitama'
 
 if __name__ == '__main__':
-    app.run(port=80)
+    app.run(debug=True, host="0.0.0.0", port=80)
